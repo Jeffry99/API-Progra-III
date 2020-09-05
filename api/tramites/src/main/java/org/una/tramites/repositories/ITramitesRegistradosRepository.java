a720993460af2c91a23ce85cedbe2f4129bdd9cc
Lucho321 <54681195+Lucho321@users.noreply.github.com>
2020-09-04 20:52:55 -0600

    Tramites Registrados, Notas, Archivos relacionados, Requisitos presentados

---
+++ api/tramites/src/main/java/org/una/tramites/repositories/ITramitesRegistradosRepository.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.una.tramites.repositories;
+
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.una.tramites.entities.TramitesRegistrados;
+import org.una.tramites.entities.Usuario;
+
+/**
+ *
+ * @author Luis
+ */
+public interface ITramitesRegistradosRepository extends JpaRepository<TramitesRegistrados, Long>{
+    
+    @Query("SELECT t FROM TramitesRegistrados t LEFT JOIN t.cliente d WHERE t.cliente.id =:id")
+    public List<TramitesRegistrados> findByClientesId(Long id);
+    public List<TramitesRegistrados> findByTramitesTiposId(Long id);
+}
